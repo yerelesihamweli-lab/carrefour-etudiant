111824808eb68caedb9568021dfd2507ecab08d7
yerelesihamweli-lab <yerelesihamweli@gmail.com>
2026-06-27 00:40:42 +0000

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+
+// Enregistrement du service worker (PWA)
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .catch((err) => console.error('Service worker :', err))
+  })
+}
